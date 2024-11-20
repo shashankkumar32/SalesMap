d35ae2f5d76312101b5185c43aa8bfea2d336ab3
shashankkumar32 <shashankofficial3209@gmail.com>
2024-11-20 11:36:46 +0530

    update-ui-1

--- app/components/global/navbar.tsx
+++ app/components/global/navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = async (props: Props) => {
       </aside>
       <nav className="absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] hidden md:block">
         <ul className="flex items-center gap-4 list-none">
-          <li>
+          {/* <li>
             <Link href="#">Products</Link>
           </li>
           <li>
@@ -34,7 +34,7 @@ const Navbar = async (props: Props) => {
           </li>
           <li>
             <Link href="#">Enterprise</Link>
-          </li>
+          </li> */}
         </ul>
       </nav>
       <aside className="flex items-center gap-4">
@@ -44,10 +44,10 @@ const Navbar = async (props: Props) => {
         >
           <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
           <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-            {/* {user ? 'Dashboard' : 'Get Started'} */}fdfdf
+            {/* {user ? 'Dashboard' : 'Get Started'} */}GO to Dashboard
           </span>
         </Link>
-        {/* {user ? <UserButton afterSignOutUrl="/" /> : null} */}button
+        {/* {user ? <UserButton afterSignOutUrl="/" /> : null} */}
         <MenuIcon className="md:hidden" />
       </aside>
     </header>
